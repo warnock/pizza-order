4418723992033d61332645acdf3bbbaa629a78a1
Megan Warnock <megandwarnock@gmail.com>
2017-03-03 09:42:48 -0800

    Add prototypes and reach specifications 2 and 3.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-function Pizza(size, topping) {
+function Pizza(size, toppingOne, toppingTwo, toppingThree) {
   this.size = size;
-  this.topping = topping;
+  this.toppingOne = toppingOne;
+  this.toppingTwo = toppingTwo
+  this.toppingThree = toppingThree
   this.value = 0;
+}
+Pizza.prototype.totalCost = function (){
+  this.pizzaSize();
+  this.meatTopping();
+  this.veggieTopping();
+  this.extraCheese();
+  return this.value;
 }
 Pizza.prototype.pizzaSize = function () {
   if (this.size === "small") {
@@ -10,5 +19,24 @@ Pizza.prototype.pizzaSize = function () {
     this.value += 12;
   } else if (this.size === "large") {
     this.value += 15;
+  }
+}
+Pizza.prototype.meatTopping = function () {
+  if (this.toppingOne === "pepperoni") {
+    this.value += 2;
+  } else {
+    this.value += 5;
+  }
+}
+Pizza.prototype.veggieTopping = function () {
+  if (this.toppingTwo === "allTheVeggies") {
+    this.value += 3;
+  }else {
+    this.value += 1;
+  }
+}
+Pizza.prototype.extraCheese = function () {
+  if (this.toppingThree === "extra cheese"){
+    this.value += 2;
   }
 }
